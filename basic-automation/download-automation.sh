cd2d27cc8ff7d19450b3242a1adcc6ed132a4de3
pakoti <lovesong7zip@gmail.com>
2021-05-31 01:06:12 +0430

    first script just added

---
+++ basic-automation/download-automation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#edit this bash file and put all downdload links in here
+
+download_links="  "
+
+
+for i in $download_links
+
+do
+
+wget $i
+
+done
+
+echo "[*]Task Completed Succesfully"
